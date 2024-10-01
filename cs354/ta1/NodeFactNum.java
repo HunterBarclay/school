d210dafe2c402be304aea08e1cfe45ca32c10a84
KyroVibe <hunter.barclay18@gmail.com>
2024-10-01 00:14:01 -0600

    ta1(wip): All but documentation and negations are done. Have added a number of tests with more to follow with negation included.

---
+++ cs354/ta1/NodeFactNum.java
@@ -0,0 +1,15 @@
+public class NodeFactNum extends NodeFact {
+
+	private String num;
+
+	public NodeFactNum(String num) {
+		this.num=num;
+	}
+
+	public double eval(Environment env) throws EvalException {
+		return Double.parseDouble(num);
+	}
+
+	public String code() { return num.contains(".") ? num : num + ".0"; }
+
+}
