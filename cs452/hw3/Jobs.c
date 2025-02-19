26d9b0595ce9ec11977fb8e93a7a86362ea48261
KyroVibe <hunter.barclay18@gmail.com>
2025-02-19 12:52:14 -0700

    cs452: Hw3 setup

---
+++ cs452/hw3/Jobs.c
@@ -0,0 +1,20 @@
+#include "deq/deq.h"
+
+#include "Jobs.h"
+#include "error.h"
+
+extern Jobs newJobs() {
+  return deq_new();
+}
+
+extern void addJobs(Jobs jobs, Pipeline pipeline) {
+  deq_tail_put(jobs,pipeline);
+}
+
+extern int sizeJobs(Jobs jobs) {
+  return deq_len(jobs);
+}
+
+extern void freeJobs(Jobs jobs) {
+  deq_del(jobs,freePipeline);
+}
---
+++ cs452/hw3/Sequence.c
@@ -0,0 +1,22 @@
+#include "deq/deq.h"
+
+#include "Sequence.h"
+#include "error.h"
+
+extern Sequence newSequence() {
+  return deq_new();
+}
+
+extern void addSequence(Sequence sequence, Pipeline pipeline) {
+  deq_tail_put(sequence,pipeline);
+}
+
+extern void freeSequence(Sequence sequence) {
+  deq_del(sequence,freePipeline);
+}
+
+extern void execSequence(Sequence sequence, Jobs jobs, int *eof) {
+  while (deq_len(sequence) && !*eof)
+    execPipeline(deq_head_get(sequence),jobs,eof);
+  freeSequence(sequence);
+}
